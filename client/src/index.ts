a901d1248ab372565a3d9367eaf95e96fdade562
lwalton101 <waltonfamily01@outlook.com>
2023-04-30 09:56:13 +0100

    added basic website and api

---
+++ client/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path"
+const app = express();
+const webPort = 8081; 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(webPort, () => {
+    console.log("Express ready");
+})
+
+app.get("/", (req, res) => {
+    res.sendFile('index.html');
+})
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,21 @@
 import WebSocketServer from "ws"
-
+import express from "express"
+var cors = require("cors");
+const app = express();
+const apiPort = 8080; 
 
 const wss : WebSocketServer.Server = new WebSocketServer.Server({port: 25565});
 
 wss.on("connection", (ws, message) => {
     console.log("ws connected");
+});
+
+app.use(cors())
+app.get("/online", (req, res) => {
+    res.send("true");
+});
+
+app.listen(apiPort, () => {
+    console.log("express online");
 });
 
